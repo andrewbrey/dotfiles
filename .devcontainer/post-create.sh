3d9c610988ee406f8e54a9d31e2bc067c5b5d545
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-11-20 18:20:29 -0800

    feat: setup more devcontainer post-create logic

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
-set -euo -pipefail
+set -euo pipefail
 
-[ -n $REMOTE_CONTAINERS ] && script/bootstrap
+if [ -n $REMOTE_CONTAINERS ]; then
+	this_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)
+	workspace_root=$(realpath ${this_dir}/..)
+
+	# =====
+	# all containerized environments
+	# =====
+	#
+	# execute the bootstrap script just as happens
+	# in GitHub Codespaces and Gitpod
+	"${workspace_root}/script/bootstrap"
+
+	# =====
+	# devcontainer specific
+	# =====
+	#
+	# perform additional setup that is unique to
+	# this devcontainer, and which doesn't happen
+	# in cloud editors
+
+	#
+	# install node and npm globals
+	echo todo, install node and npm globals...
+
+	unset this_dir
+	unset workspace_root
+fi
