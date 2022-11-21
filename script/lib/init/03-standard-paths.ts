f3db56da59f28e5814bc358261679a6fbafb4052
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-11-20 17:45:10 -0800

    feat: create .extra at bootstrap

--- script/lib/init/03-standard-paths.ts
+++ script/lib/init/03-standard-paths.ts
@@ -19,6 +19,28 @@ for (const [name, path] of Object.entries(env.STANDARD_DIRS)) {
   await $.fs.ensureDir(path);
 }
 
+$.logStep("step: place standard files");
+
+await $.fs.ensureFile(`${env.HOME}/.hushlogin`);
+await $.fs.ensureDir(`${env.HOME}/.dots`);
+
+const dotExtra = `${env.HOME}/.dots/.extra`;
+if (!(await $.exists(dotExtra))) {
+  await Deno.writeTextFile(
+    dotExtra,
+    `#!/usr/bin/env zsh
+
+# =====
+# chezmoi ignored
+# =====
+#
+# this file can contain shell-exports (variables, functions, and aliases) that
+# are ignored by dotfiles management.
+
+`,
+  );
+}
+
 $.logGroupEnd();
 
 doneWith(id);
