fc83919858c70b19774bf42b6e97f1a42a2f3189
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-11-30 15:44:35 -0800

    feat: add uninstall for bat

--- script/lib/apps/bat/uninstall.ts
+++ script/lib/apps/bat/uninstall.ts
@@ -1,5 +1,21 @@
 #!/usr/bin/env -S deno run --allow-env --allow-net=example.com --allow-read --allow-write --allow-run
 
-import { osInvariant } from "../../mod.ts";
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants } from "../_cli/pamkit.ts";
 
 osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+
+const isInstalled = typeof (await $.which("bat")) !== "undefined";
+if (isInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew uninstall bat`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo apt purge -y bat`;
+  }
+}
+
+if (await $.exists(dotAppPath)) {
+  await Deno.remove(dotAppPath, { recursive: true });
+}
