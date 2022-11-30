6123cca4a6e1cc68a2e3895680a084b84e5e35c0
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-11-29 23:18:58 -0800

    feat: add gh installer

---
+++ script/lib/apps/gh/install.ts
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land,api.github.com,github.com,objects.githubusercontent.com --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, invariant, osInvariant } from "../../mod.ts";
+import {
+  APP_ARTIFACTS_DIR,
+  GH_RELEASE_INFO_NAME,
+  ghReleaseLatestInfo,
+  InstallerMeta,
+  META_MANIFEST_NAME,
+  streamDownload,
+} from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), APP_ARTIFACTS_DIR);
+
+if (env.OS === "darwin") {
+  await $`brew install gh`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+} else {
+  const releaseInfoPath = $.path.join(dotAppPath, GH_RELEASE_INFO_NAME);
+  const debInstallerPath = $.path.join(dotAppPath, "gh.deb");
+
+  await $.fs.ensureDir(dotAppPath);
+
+  const releaseInfo = await ghReleaseLatestInfo("cli", "cli");
+  await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
+
+  const { assets, tag_name } = releaseInfo;
+  const latestVersion = tag_name.split("v")[1] ?? "0.0.0";
+  const targetName = `gh_${latestVersion}_linux_amd64.deb`;
+  const targetAsset = assets.find((a) => a.name === targetName);
+
+  invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
+
+  await streamDownload(targetAsset.browser_download_url, debInstallerPath);
+
+  await $`sudo apt install -y ${debInstallerPath}`;
+}
+
+const versionOutput = await $`gh --version`.text(); // gh version 2.20.2-17-g2d61515a (2022-11-17)
+const version = versionOutput.split(" ")[2].split("-")[0];
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-manual",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, META_MANIFEST_NAME);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
