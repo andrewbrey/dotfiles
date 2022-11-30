3d357f314f76de71c52162031d6fcf29f0603659
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-11-29 22:15:06 -0800

    fix: remove debug wrapper

--- script/lib/apps/bat/install.ts
+++ script/lib/apps/bat/install.ts
@@ -22,21 +22,19 @@ if (env.OS === "darwin") {
 
   await $.fs.ensureDir(dotAppPath);
 
-  if (Math.random() > 2) {
-    const releaseInfo = await ghReleaseLatestInfo("sharkdp", "bat");
-    await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
+  const releaseInfo = await ghReleaseLatestInfo("sharkdp", "bat");
+  await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
 
-    const { assets, tag_name } = releaseInfo;
-    const latestVersion = tag_name.split("v")[1] ?? "0.0.0";
-    const targetName = `bat_${latestVersion}_amd64.deb`;
-    const targetAsset = assets.find((a) => a.name === targetName);
+  const { assets, tag_name } = releaseInfo;
+  const latestVersion = tag_name.split("v")[1] ?? "0.0.0";
+  const targetName = `bat_${latestVersion}_amd64.deb`;
+  const targetAsset = assets.find((a) => a.name === targetName);
 
-    invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
+  invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
 
-    await streamDownload(targetAsset.browser_download_url, debInstallerPath);
+  await streamDownload(targetAsset.browser_download_url, debInstallerPath);
 
-    await $`sudo apt install -y ${debInstallerPath}`;
-  }
+  await $`sudo apt install -y ${debInstallerPath}`;
 }
 
 const versionOutput = await $`bat --version`.text(); // bat 0.22.1 (e5d9579)
