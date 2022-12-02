2702af513f2ca953896482c7255f988f4c0edce7
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-01 21:10:34 -0800

    feat: add removal script to npm-globals

---
+++ script/lib/apps/npm-globals/remove.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, colors, dedent, env, osInvariant } from "../../mod.ts";
+import { constants } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+
+$.logGroup(() => {
+  $.logWarn(
+    "warn:",
+    dedent(`
+			actual node_module lib & bin files are being left untouched
+			because there is no good way to know which have been added
+			or removed since initial installation - remove them manually
+			within ${colors.magenta(env.STANDARD_DIRS.NPM_INSTALL)}.
+		`),
+  );
+});
+
+if (await $.exists(dotAppPath)) {
+  await Deno.remove(dotAppPath, { recursive: true });
+}
