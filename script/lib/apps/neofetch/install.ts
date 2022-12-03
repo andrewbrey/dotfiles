1ae555420bc250abd875316045589bca205af294
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-03 04:57:08 +0000

    feat: add neofetch app scripts

---
+++ script/lib/apps/neofetch/install.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("neofetch")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install neofetch`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo apt install -y neofetch`;
+  }
+}
+
+const versionOutput = await $`neofetch --version`.noThrow().text(); // Neofetch 7.1.0
+const version = versionOutput.split(" ")[1];
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/neofetch/update.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deb.nodesource.com,deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, dedent, osInvariant } from "../../mod.ts";
+import { constants, getInstallerMetas } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+$.logGroup(() => {
+  $.logWarn(
+    "warn:",
+    dedent(`
+			installation is managed; skipping manual update
+
+		`),
+  );
+});
+
+const versionOutput = await $`neofetch --version`.noThrow().text(); // Neofetch 7.10.0
+const version = versionOutput.split("v")[1];
+
+const [meta] = await getInstallerMetas(new Set(["neofetch"]));
+meta.version = version;
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
