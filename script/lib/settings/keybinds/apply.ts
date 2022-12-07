74abe24ecb2a0954b4cd5979ab64a9d7943cd077
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-07 00:00:01 -0800

    feat: implement keybind apply

--- script/lib/settings/keybinds/apply.ts
+++ script/lib/settings/keybinds/apply.ts
@@ -1,9 +1,28 @@
 #!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
 
-import { getChezmoiData, osInvariant } from "../../mod.ts";
+import { $, dedent, env, getChezmoiData, invariant, osInvariant } from "../../mod.ts";
 
 osInvariant();
 
 const chezmoiData = await getChezmoiData();
 
-console.log(chezmoiData);
+if (env.OS === "darwin") {
+  $.logGroup(() => {
+    $.logWarn(
+      "warn:",
+      dedent(`
+				skipping media key settings for mac
+
+			`),
+    );
+  });
+} else {
+  if (!env.IN_CONTAINER && (chezmoiData.is_popos || chezmoiData.is_ubuntu)) {
+    invariant(typeof (await $.which("dconf")) !== "undefined", "dconf is required");
+
+    const loadKey = "/org/gnome/settings-daemon/plugins/media-keys/";
+    const dconfSrc = `${env.STANDARD_DIRS.DOT_DOTS_SETTINGS}/keybinds/.keybinds.dconf`;
+
+    await $`dconf load ${loadKey}`.stdin(await Deno.readTextFile(dconfSrc));
+  }
+}
