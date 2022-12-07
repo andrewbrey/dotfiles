83af9cd59fe325a4b7986c6074a31768faeb0a85
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-07 00:18:29 -0800

    docs: add invariant and help message

--- script/lib/settings/keybinds/apply.ts
+++ script/lib/settings/keybinds/apply.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
 
-import { $, dedent, env, getChezmoiData, invariant, osInvariant } from "../../mod.ts";
+import { $, colors, dedent, env, getChezmoiData, invariant, osInvariant } from "../../mod.ts";
 
 osInvariant();
 
@@ -19,6 +19,10 @@ if (env.OS === "darwin") {
 } else {
   if (!env.IN_CONTAINER && (chezmoiData.is_popos || chezmoiData.is_ubuntu)) {
     invariant(typeof (await $.which("dconf")) !== "undefined", "dconf is required");
+    invariant(
+      typeof (await $.which("wmctrl")) !== "undefined",
+      `wmctrl is required, install it with ${colors.magenta("pam install -a wmctrl")}`,
+    );
 
     const loadKey = "/org/gnome/settings-daemon/plugins/media-keys/";
     const dconfSrc = `${env.STANDARD_DIRS.DOT_DOTS_SETTINGS}/keybinds/.keybinds.dconf`;
