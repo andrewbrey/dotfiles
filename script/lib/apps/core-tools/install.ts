5f4b3f0c987fbc80715dbab0db03273847eee2a4
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-08 04:18:56 +0000

    feat: fill out core-tools

--- script/lib/apps/core-tools/install.ts
+++ script/lib/apps/core-tools/install.ts
@@ -1,16 +1,46 @@
 #!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
 
-import { $, $dirname, env, invariant, osInvariant } from "../../mod.ts";
-import { constants, ghReleaseLatestInfo, InstallerMeta, streamDownload } from "../_cli/pamkit.ts";
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
 
 osInvariant();
 
 const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
 await $.fs.ensureDir(dotAppPath);
 
+const ack = env.OS === "darwin" ? "ack" : "ack";
+const curl = env.OS === "darwin" ? "curl" : "curl";
+const direnv = env.OS === "darwin" ? "direnv" : "direnv";
+const exiftool = env.OS === "darwin" ? "exiftool" : "libimage-exiftool-perl";
+const fzf = env.OS === "darwin" ? "fzf" : "fzf";
+const gnupg = env.OS === "darwin" ? "gnupg" : "gnupg";
+const htop = env.OS === "darwin" ? "htop" : "htop";
+const jq = env.OS === "darwin" ? "jq" : "jq";
+const nano = env.OS === "darwin" ? "nano" : "nano";
+const p7zip = env.OS === "darwin" ? "p7zip" : "p7zip-full";
+const sqlite = env.OS === "darwin" ? "sqlite" : "sqlite3";
+const tree = env.OS === "darwin" ? "tree" : "tree";
+const vim = env.OS === "darwin" ? "vim" : "vim";
+const w3m = env.OS === "darwin" ? "w3m" : "w3m";
 const xz = env.OS === "darwin" ? "xz" : "xz-utils";
 
-const list = [xz];
+const list = [
+  ack,
+  curl,
+  direnv,
+  exiftool,
+  fzf,
+  gnupg,
+  htop,
+  jq,
+  nano,
+  p7zip,
+  sqlite,
+  tree,
+  vim,
+  w3m,
+  xz,
+];
 if (env.OS === "darwin") {
   await $`brew install ${list}`.env({ HOMEBREW_NO_ANALYTICS: "1" });
 } else {
