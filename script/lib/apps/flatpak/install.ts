2750a7eda28e9a38e11cb942a963585736a1b363
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-08 13:30:08 -0800

    feat: add flatpak

---
+++ script/lib/apps/flatpak/install.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("flatpak")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "linux") {
+    await $`sudo apt install -y flatpak`;
+    await $`flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo`;
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version: "",
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
