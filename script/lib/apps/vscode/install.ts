650d4a3abb0f8463892a648abfb1999497b19d43
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-08 15:41:07 -0800

    feat: add vscode

---
+++ script/lib/apps/vscode/install.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, colors, env, invariant, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("code")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask visual-studio-code`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    invariant(
+      typeof (await $.which("snap")) !== "undefined",
+      `snap is required, install it with ${colors.magenta("pam install -a snapd")}`,
+    );
+
+    await $`sudo snap install code --classic`;
+  }
+}
+
+const versionOutput = await $`code --version`.lines(); // 1.74.0\n.....
+const version = versionOutput?.at(0) ?? "";
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
