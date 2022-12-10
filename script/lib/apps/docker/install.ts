41679701fef27a077d301490ed6226d3796ce2e8
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-09 17:33:38 -0800

    feat: add docker

---
+++ script/lib/apps/docker/install.ts
@@ -0,0 +1,61 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("docker")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask docker`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo mkdir -p /etc/apt/keyrings`;
+    await $`sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg`.stdin(
+      await $`curl -fsSL https://download.docker.com/linux/ubuntu/gpg`.text(),
+    );
+
+    const arch = await $`dpkg --print-architecture`.text();
+    const codename = await $`lsb_release -cs`.text();
+
+    await $`sudo tee /etc/apt/sources.list.d/docker.list`.stdin(
+      await $
+        .raw`echo "deb [arch=${arch} signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu ${codename} stable"`
+        .text(),
+    );
+
+    await $`sudo apt update`;
+    await $`sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin`;
+
+    const dockerGroupExists = (await $`grep -q "^docker:" /etc/group`.noThrow()).code === 0;
+    if (!dockerGroupExists) {
+      await $`sudo groupadd docker`;
+    }
+
+    const usersGroupsRaw = await $`id -nGz $USER`.text();
+    const usersGroups = await $`tr '\\0' '\\n'`.stdin(usersGroupsRaw).text();
+    const userInGroup = (await $`grep -q '^docker$'`.stdin(usersGroups).noThrow()).code === 0;
+    if (!userInGroup) {
+      await $`sudo usermod -aG docker $USER`;
+    }
+
+    $.logWarn("warn:", "you need to log out and back in to enable sudo-less docker access");
+  }
+}
+
+const versionOutput = await $`docker --version`.text(); // Docker version 20.10.21, build baeda1f
+const version = versionOutput.split(" ")?.at(2)?.split(",")?.at(0) ?? "";
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
