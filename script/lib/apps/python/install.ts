f1939877c246f6ac1e919d9de5e722fee7a4ca29
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-11 00:11:07 +0000

    feat: add python

---
+++ script/lib/apps/python/install.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const pythonNotInstalled = typeof (await $.which("python3")) === "undefined";
+const pipNotInstalled = typeof (await $.which("pip3")) === "undefined";
+if (pythonNotInstalled || pipNotInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install python3`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo apt install -y python3 python3-pip`;
+  }
+}
+
+const versionOutput = await $`python3 --version`.text(); // Python 3.10.6
+const version = versionOutput.split(" ")?.at(1) ?? "";
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
