d6d4018eaeaf4aa0e5ba1622653f0150389e04a0
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-11 07:01:01 +0000

    feat: add gnome-screenshot

---
+++ script/lib/apps/gnome-screenshot/install.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+let version = ""; // not installed on mac, so default version
+const notInstalled = typeof (await $.which("gnome-screenshot")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "linux") {
+    await $`sudo apt install -y gnome-screenshot`;
+
+    const versionOutput = await $`gnome-screenshot --version`.text(); // gnome-screenshot 41.0
+    version = versionOutput?.split(" ")?.at(1) ?? "";
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
