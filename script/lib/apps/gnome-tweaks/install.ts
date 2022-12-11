5572f067fe2491b4f6ad73a6e77f74d5e92f8903
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-11 06:59:01 +0000

    feat: add gnome-tweaks

---
+++ script/lib/apps/gnome-tweaks/install.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+let version = ""; // not installed on mac, so default version
+const notInstalled = typeof (await $.which("gnome-tweaks")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "linux") {
+    await $`sudo apt install -y gnome-tweaks`;
+
+    version = await $`gnome-tweaks --version`.text(); // 42.beta
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
