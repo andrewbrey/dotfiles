4a48fc19a904cdb689f25048a0ab7f6944855361
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-11 18:01:26 +0000

    feat: add gpick

---
+++ script/lib/apps/gpick/remove.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+
+const isInstalled = typeof (await $.which("gpick")) !== "undefined";
+if (isInstalled) {
+  if (env.OS === "linux") {
+    await $`sudo apt purge -y gpick`;
+  }
+}
+
+if (await $.exists(dotAppPath)) {
+  await Deno.remove(dotAppPath, { recursive: true });
+}
