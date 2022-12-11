7156773ac45f646c276a0a36ae58e45663e72dc9
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-11 18:07:18 +0000

    feat: add gimp

---
+++ script/lib/apps/gimp/install.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("gimp")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask gimp`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo apt install -y gimp`;
+  }
+}
+
+const versionOutput = await $`gimp --version`.text(); // GNU Image Manipulation Program version 2.10.30
+const version = versionOutput.split("v")?.at(5) ?? "";
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
