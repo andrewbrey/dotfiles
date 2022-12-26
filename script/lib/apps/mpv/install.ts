7940a52ca730d8af72894bec884f9418bcf2879a
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-26 12:16:24 -0800

    feat: add mpv

---
+++ script/lib/apps/mpv/install.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("mpv")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask mpv`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo apt install -y mpv`;
+  }
+}
+
+const versionOutput = await $`mpv --version`.text(); // mpv 0.34.1 Copyright...
+const version = versionOutput.split(" ")?.at(1) ?? "";
+
+const meta: InstallerMeta = {
+  name: $.path.basename($dirname(import.meta.url)),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/mpv/remove.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+
+const isInstalled = typeof (await $.which("mpv")) !== "undefined";
+if (isInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew uninstall --cask mpv`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo apt purge -y mpv`;
+  }
+}
+
+if (await $.exists(dotAppPath)) {
+  await Deno.remove(dotAppPath, { recursive: true });
+}
