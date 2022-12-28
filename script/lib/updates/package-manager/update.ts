d566168c4473ebc7fcfd800714f00907332707b5
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-28 14:02:34 -0800

    feat: implement package manager update

---
+++ script/lib/updates/package-manager/update.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
+
+import { $, env, osInvariant } from "../../mod.ts";
+
+osInvariant();
+
+if (env.OS === "darwin") {
+  await $`brew update`;
+  await $`brew upgrade`;
+  await $`brew cleanup`;
+  await $`brew autoremove`;
+  await $`brew doctor`;
+} else {
+  await $`sudo apt update`;
+  await $`sudo apt upgrade -y`;
+  await $`sudo apt autoremove -y`;
+
+  if (typeof (await $.which("snap")) !== "undefined") {
+    await $`sudo snap refresh`;
+  }
+
+  if (typeof (await $.which("flatpak")) !== "undefined") {
+    await $`flatpak update -y`;
+  }
+}
