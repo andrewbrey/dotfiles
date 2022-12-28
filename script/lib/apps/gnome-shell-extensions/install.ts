d87beb6e5ce22bb6bf11b54d5a751a2b8bf9571a
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-27 16:06:26 -0800

    feat: partial install implementation

--- script/lib/apps/gnome-shell-extensions/install.ts
+++ script/lib/apps/gnome-shell-extensions/install.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
 
-import { $, $dirname, getChezmoiData, invariant, osInvariant } from "../../mod.ts";
+import { $, $dirname, env, getChezmoiData, invariant, osInvariant } from "../../mod.ts";
 import { constants, InstallerMeta } from "../_cli/pamkit.ts";
 
 osInvariant();
@@ -16,6 +16,40 @@ if (!chezmoiData.is_containerized && (chezmoiData.is_popos || chezmoiData.is_ubu
     typeof (await $.which("gnome-extensions")) !== "undefined",
     "gnome-extensions is required",
   );
+  type ConfOnlyManifest = { dconf: string; confOnly: boolean };
+  type UrlManifest = { dconf: string; url: string };
+  type GnomeExtensionMeta = {
+    name: string;
+    manifestPath: string;
+    manifest: ConfOnlyManifest | UrlManifest;
+  };
+
+  const extDir = $.path.join(env.STANDARD_DIRS.DOT_DOTS_APPS, "gnome-shell-extensions");
+  const extMetas: GnomeExtensionMeta[] = [];
+
+  for await (const d of Deno.readDir(extDir)) {
+    if (d.isDirectory) {
+      const extJson = JSON.parse(
+        await Deno.readTextFile($.path.join(extDir, d.name, "ext.json")),
+      ) as GnomeExtensionMeta["manifest"];
+
+      const meta: GnomeExtensionMeta = {
+        name: d.name,
+        manifestPath: $.path.join(extDir, d.name),
+        manifest: ("url" in extJson)
+          ? { dconf: extJson.dconf, url: extJson.url }
+          : { dconf: extJson.dconf, confOnly: extJson.confOnly },
+      };
+
+      extMetas.push(meta);
+    }
+  }
+
+  const fullGnomeVersion = await $`gnome-shell --version 2>/dev/null`.text(); // GNOME Shell 3.38.4 (or GNOME Shell 40.5)
+  const gnomeVersion = fullGnomeVersion.split(" ")?.at(2)?.split(".")?.slice(0, 2)?.join(".") ?? ""; // 3.38 or 40.5
+
+  invariant(gnomeVersion.length > 0, "unable to determine gnome version");
+
   // TODO: install extensions
 }
 
