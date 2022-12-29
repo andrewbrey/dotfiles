5ae54c8dd3f6cdcd55ec88ed6507a85239f8c014
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-12-29 12:31:31 -0800

    fix: ensure rev-parse happens in dotfiles

--- script/lib/updates/dotfiles/updater.ts
+++ script/lib/updates/dotfiles/updater.ts
@@ -1,18 +1,24 @@
 #!/usr/bin/env -S deno run --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
 
-import { $, invariant, osInvariant } from "../../mod.ts";
+import { $, $dirname, invariant, osInvariant } from "../../mod.ts";
 
 osInvariant();
 
-const gitRoot = await $`git rev-parse --show-toplevel`.text();
-const dotfileRepoStatus = (await $`git -C ${gitRoot} status --porcelain`.lines()).filter(Boolean);
+const gitRoot = await $`git rev-parse --show-toplevel`.cwd(
+  $dirname(import.meta.url),
+).text();
+const dotfileRepoStatus =
+  (await $`git -C ${gitRoot} status --porcelain`.lines()).filter(Boolean);
 
 const OK_TO_BE_DIRTY: string[] = [];
 const worthWarningAbout = dotfileRepoStatus.filter((stl) =>
   !OK_TO_BE_DIRTY.some((ok) => stl.includes(ok))
 );
 
-invariant(worthWarningAbout.length === 0, "dotfiles repo has changes that need to be committed");
+invariant(
+  worthWarningAbout.length === 0,
+  "dotfiles repo has changes that need to be committed",
+);
 
 const { code: chezmoiStatus } = await $`chezmoi verify`.noThrow();
 
