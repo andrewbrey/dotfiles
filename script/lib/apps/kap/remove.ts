94b6da331e1ebf0e35c8da95fe5dc21c47d13668
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-04 09:56:55 -0800

    feat: add kap

---
+++ script/lib/apps/kap/remove.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+
+const isInstalled = typeof (await $.which("kap")) !== "undefined";
+if (isInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew uninstall --cask kap`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  }
+}
+
+if (await $.exists(dotAppPath)) {
+  await Deno.remove(dotAppPath, { recursive: true });
+}
