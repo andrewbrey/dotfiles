917b777e7f39e80c2b052fd4928552d98fec9881
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-04 10:26:36 -0800

    feat: add delta

--- README.md
+++ README.md
@@ -83,7 +83,6 @@
   ```
   https://github.com/sharkdp/fd
   https://github.com/sharkdp/hyperfine
-  https://github.com/dandavison/delta
   https://github.com/dalance/procs
   https://github.com/ajeetdsouza/zoxide
   https://github.com/zyedidia/eget
---
+++ script/lib/apps/delta/install.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, invariant, osInvariant, stripAnsi } from "../../mod.ts";
+import { constants, ghReleaseLatestInfo, InstallerMeta, streamDownload } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("delta")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install git-delta`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    const releaseInfoPath = $.path.join(dotAppPath, constants.ghReleaseInfoName);
+    const debInstallerPath = $.path.join(dotAppPath, "delta.deb");
+
+    const releaseInfo = await ghReleaseLatestInfo("dandavison", "delta");
+    await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
+
+    const { assets, tag_name } = releaseInfo;
+    const latestVersion = tag_name ?? "0.0.0";
+    const targetName = `git-delta_${latestVersion}_amd64.deb`;
+    const targetAsset = assets.find((a) => a.name === targetName);
+
+    invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
+
+    await streamDownload(targetAsset.browser_download_url, debInstallerPath);
+
+    await $`sudo apt install -y ${debInstallerPath}`;
+  }
+}
+
+const versionOutput = stripAnsi(await $`delta --version`.text()).trim(); // delta 0.15.1
+const version = versionOutput.split(" ")?.at(1) ?? "";
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
