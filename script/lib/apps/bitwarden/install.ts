c5fd3b2450e5bf3e3e17fb2053266e91ec0d7c42
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-03 18:20:14 -0800

    feat: add bitwarden

---
+++ script/lib/apps/bitwarden/install.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, invariant, osInvariant } from "../../mod.ts";
+import { constants, createAndLinkNativefierApp, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const server = Deno.env.get("SECRET_BITWARDEN_SERVER");
+invariant(
+  typeof server === "string" && server.length > 0,
+  "missing required env $SECRET_BITWARDEN_SERVER",
+);
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const name = $dirname(import.meta.url, true);
+const notInstalled = typeof (await $.which(name)) === "undefined";
+if (notInstalled) {
+  await createAndLinkNativefierApp({
+    appName: name,
+    displayName: "Bitwarden",
+    website: server,
+  });
+}
+
+const meta: InstallerMeta = {
+  name,
+  path: $dirname(import.meta.url),
+  type: "installed-manual",
+  version: "0.0.0",
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/bitwarden/update.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, invariant, osInvariant } from "../../mod.ts";
+import { constants, createAndLinkNativefierApp, getInstallerMetas } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const server = Deno.env.get("SECRET_BITWARDEN_SERVER");
+invariant(
+  typeof server === "string" && server.length > 0,
+  "missing required env $SECRET_BITWARDEN_SERVER",
+);
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const [meta] = await getInstallerMetas(new Set([$dirname(import.meta.url, true)]));
+
+const name = $dirname(import.meta.url, true);
+const installed = typeof (await $.which(name)) !== "undefined";
+if (installed) {
+  await createAndLinkNativefierApp({
+    appName: name,
+    displayName: "Bitwarden",
+    website: server,
+  });
+
+  meta.lastCheck = Date.now();
+}
+
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
