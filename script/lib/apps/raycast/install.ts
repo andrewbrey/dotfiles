55e728d11872002d437b9bc85f2a593fcd8f753d
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-04 10:01:33 -0800

    feat: add raycast

---
+++ script/lib/apps/raycast/install.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("raycast")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask raycast`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version: "",
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
