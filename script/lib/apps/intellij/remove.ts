a1fccd296f6b6458979bf01c957d3689296bf6cc
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-05 13:27:19 -0800

    feat: add intellij

---
+++ script/lib/apps/intellij/remove.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, colors, env, invariant, osInvariant } from "../../mod.ts";
+import { constants } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+
+const isInstalled = typeof (await $.which("intellij-idea-ultimate")) !== "undefined";
+if (isInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew uninstall --cask intellij-idea`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    invariant(
+      typeof (await $.which("snap")) !== "undefined",
+      `snap is required, install it with ${colors.magenta("pam install -a snapd")}`,
+    );
+
+    await $`sudo snap remove intellij-idea-ultimate`;
+  }
+}
+
+if (await $.exists(dotAppPath)) {
+  await Deno.remove(dotAppPath, { recursive: true });
+}
