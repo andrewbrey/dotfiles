fbb508ad5e4e4ebffbc5b0c3623e4a9a8bf25009
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-05 13:35:02 -0800

    feat: add vlc

---
+++ script/lib/apps/vlc/install.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, colors, env, invariant, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("vlc")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask vlc`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    invariant(
+      typeof (await $.which("snap")) !== "undefined",
+      `snap is required, install it with ${colors.magenta("pam install -a snapd")}`,
+    );
+
+    await $`sudo snap install vlc`;
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version: "",
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
