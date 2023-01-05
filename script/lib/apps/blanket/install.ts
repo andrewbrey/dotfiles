827d6739433afb60280d71b00cb549cafb309bd5
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-05 11:05:18 -0800

    feat: add blanket

---
+++ script/lib/apps/blanket/install.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, invariant, osInvariant } from "../../mod.ts";
+import { constants, flatpakAppMissing, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+invariant(typeof (await $.which("flatpak")) !== "undefined", "flatpak is required");
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = await flatpakAppMissing("Blanket");
+if (notInstalled) {
+  if (env.OS === "linux") {
+    await $`flatpak install -y flathub com.rafaelmardojai.Blanket`;
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version: "",
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
