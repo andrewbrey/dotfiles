47d46eacad07fdbafb55fd40cee3aeb70cd27973
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-05 10:03:20 -0800

    feat: add task

---
+++ script/lib/apps/task/install.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, colors, env, invariant, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("task")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install go-task`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    invariant(
+      typeof (await $.which("snap")) !== "undefined",
+      `snap is required, install it with ${colors.magenta("pam install -a snapd")}`,
+    );
+
+    await $`sudo snap install task --classic`;
+  }
+}
+
+const versionOutput = await $`task --version`.text(); // Task version: v3.19.1 (h1:2KMJk6mDBacSPuFxPFvlvvHJwGZtU/hN2ENZpaFqR5s=)
+const version = versionOutput.split(" ")?.at(2)?.split("v")?.at(1) ?? "";
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
