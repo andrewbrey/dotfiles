18b8d7bc83e70f8742765c952c892111f1ec90f9
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-06 13:55:47 -0800

    feat: add audacity

---
+++ script/lib/apps/audacity/install.ts
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, invariant, osInvariant } from "../../mod.ts";
+import {
+  constants,
+  ghReleaseLatestInfo,
+  InstallerMeta,
+  linkBinaryToUserPath,
+  linkDesktopFileForApp,
+  streamDownload,
+} from "../_cli/pamkit.ts";
+
+osInvariant();
+
+let version = "";
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("audacity")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask audacity`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    const releaseInfoPath = $.path.join(dotAppPath, constants.ghReleaseInfoName);
+    const binPath = $.path.join(dotAppPath, "audacity.AppImage");
+
+    const releaseInfo = await ghReleaseLatestInfo("audacity", "audacity");
+    await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
+
+    const { assets, tag_name } = releaseInfo;
+    const latestVersion = tag_name.split("-")?.at(1) ?? "0.0.0";
+    const targetName = `audacity-linux-${latestVersion}-x64.AppImage`;
+    const targetAsset = assets.find((a) => a.name === targetName);
+
+    invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
+
+    await streamDownload(targetAsset.browser_download_url, binPath);
+
+    await linkBinaryToUserPath(binPath, "audacity");
+    await linkDesktopFileForApp("audacity");
+
+    version = latestVersion;
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: env.OS === "darwin" ? "installed-managed" : "installed-manual",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/audacity/update.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, dedent, env, invariant, osInvariant } from "../../mod.ts";
+import {
+  constants,
+  getInstallerMetas,
+  ghReleaseLatestInfo,
+  linkBinaryToUserPath,
+  linkDesktopFileForApp,
+  streamDownload,
+} from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const [meta] = await getInstallerMetas(new Set([$dirname(import.meta.url, true)]));
+
+const installed = typeof (await $.which("audacity")) !== "undefined";
+if (installed) {
+  if (env.OS === "darwin") {
+    $.logGroup(() => {
+      $.logWarn(
+        "warn:",
+        dedent(`
+    			installation is managed; skipping manual update
+
+    		`),
+      );
+    });
+  } else {
+    const releaseInfoPath = $.path.join(dotAppPath, constants.ghReleaseInfoName);
+    const binPath = $.path.join(dotAppPath, "audacity.AppImage");
+
+    const releaseInfo = await ghReleaseLatestInfo("audacity", "audacity");
+    await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
+
+    const { assets, tag_name } = releaseInfo;
+    const latestVersion = tag_name.split("-")?.at(1) ?? "0.0.0";
+    const targetName = `audacity-linux-${latestVersion}-x64.AppImage`;
+    const targetAsset = assets.find((a) => a.name === targetName);
+
+    invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
+
+    await streamDownload(targetAsset.browser_download_url, binPath);
+
+    await linkBinaryToUserPath(binPath, "audacity");
+    await linkDesktopFileForApp("audacity");
+
+    meta.version = latestVersion;
+    meta.lastCheck = Date.now();
+  }
+}
+
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
