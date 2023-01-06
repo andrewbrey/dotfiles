e48a9746be1627108e0e5b2929727dc76417eca2
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-06 14:01:56 -0800

    feat: add obsidian

---
+++ script/lib/apps/obsidian/install.ts
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, invariant, osInvariant } from "../../mod.ts";
+import {
+  constants,
+  ghReleaseLatestInfo,
+  InstallerMeta,
+  linkBinaryToUserPath,
+  linkDesktopFileForApp,
+  streamDownload,
+} from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+let version = "";
+const notInstalled = typeof (await $.which("obsidian")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask obsidian`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    const releaseInfoPath = $.path.join(dotAppPath, constants.ghReleaseInfoName);
+    const binPath = $.path.join(dotAppPath, "obsidian.AppImage");
+
+    const releaseInfo = await ghReleaseLatestInfo("obsidianmd", "obsidian-releases");
+    await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
+
+    const { assets, tag_name } = releaseInfo;
+    const latestVersion = tag_name.split("v")?.at(1) ?? "0.0.0";
+    const targetName = `Obsidian-${latestVersion}.AppImage`;
+    const targetAsset = assets.find((a) => a.name === targetName);
+
+    invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
+
+    await streamDownload(targetAsset.browser_download_url, binPath);
+
+    await linkBinaryToUserPath(binPath, "obsidian");
+    await linkDesktopFileForApp("obsidian");
+
+    version = latestVersion;
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: env.OS === "darwin" ? "installed-managed" : "installed-manual",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/obsidian/outdated.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { getInstallerMetas, ghReleaseLatestInfo, wrapOutdatedCheck } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const [meta] = await getInstallerMetas(new Set([$dirname(import.meta.url, true)]));
+
+const outdatedCheck = await wrapOutdatedCheck(meta, 3, async () => {
+  if (env.OS === "darwin") {
+    return ""; // managed on darwin
+  } else {
+    const releaseInfo = await ghReleaseLatestInfo("obsidianmd", "obsidian-releases");
+    const { tag_name } = releaseInfo;
+    const latest = tag_name.split("v")?.at(1) ?? "";
+
+    return latest;
+  }
+});
+
+await $`echo ${JSON.stringify(outdatedCheck)}`.printCommand(false);
