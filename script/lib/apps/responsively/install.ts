a70ce683cd7394efecc3beb05ee83b9649d1c52e
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-06 13:48:54 -0800

    feat: add responsively

---
+++ script/lib/apps/responsively/install.ts
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, invariant, osInvariant } from "../../mod.ts";
+import {
+  constants,
+  ghReleaseLatestInfo,
+  InstallerMeta,
+  linkBinaryToUserPath,
+  linkDesktopFileForApp,
+  streamDownload,
+} from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+let version = "";
+const notInstalled = typeof (await $.which("responsively")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install --cask responsively`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    const releaseInfoPath = $.path.join(dotAppPath, constants.ghReleaseInfoName);
+    const binPath = $.path.join(dotAppPath, "responsively.AppImage");
+
+    const releaseInfo = await ghReleaseLatestInfo("responsively-org", "responsively-app");
+    await Deno.writeTextFile(releaseInfoPath, JSON.stringify(releaseInfo, null, 2));
+
+    const { assets, tag_name } = releaseInfo;
+    const latestVersion = tag_name.split("v")?.at(1) ?? "0.0.0";
+    const targetName = `ResponsivelyApp-${latestVersion}.AppImage`;
+    const targetAsset = assets.find((a) => a.name === targetName);
+
+    invariant(typeof targetAsset !== "undefined", "no suitable installation target found");
+
+    await streamDownload(targetAsset.browser_download_url, binPath);
+
+    await linkBinaryToUserPath(binPath, "responsively");
+    await linkDesktopFileForApp("responsively");
+
+    version = latestVersion;
+  }
+}
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: env.OS === "darwin" ? "installed-managed" : "installed-manual",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
