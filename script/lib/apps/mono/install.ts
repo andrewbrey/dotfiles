95faf96ac64b6c8df697602d7d28a078ed0288f6
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-06 12:04:34 -0800

    feat: add mono

---
+++ script/lib/apps/mono/install.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $, $dirname, env, osInvariant } from "../../mod.ts";
+import { constants, InstallerMeta } from "../_cli/pamkit.ts";
+
+osInvariant();
+
+const dotAppPath = $.path.join($dirname(import.meta.url), constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const notInstalled = typeof (await $.which("mono")) === "undefined";
+if (notInstalled) {
+  if (env.OS === "darwin") {
+    await $`brew install mono`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+  } else {
+    await $`sudo apt install -y mono-devel`;
+  }
+}
+
+const versionOutput = await $`mono --version`.lines(); // Mono JIT compiler version 6.8.0.105....\n
+const version = versionOutput?.at(0)?.split(" ")?.at(4) ?? "";
+
+const meta: InstallerMeta = {
+  name: $dirname(import.meta.url, true),
+  path: $dirname(import.meta.url),
+  type: "installed-managed",
+  version,
+  lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
