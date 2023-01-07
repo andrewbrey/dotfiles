80e6c81ca1014bb9f134570798115d8460a2ccae
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-06 20:09:49 -0800

    refactor: remove ytdl updater

--- script/lib/updates/youtube-dl/updater.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net=deno.land --allow-read --allow-write --allow-run
-
-import { $, env, osInvariant } from "../../mod.ts";
-
-osInvariant();
-
-if (typeof (await $.which("youtube-dl")) !== "undefined") {
-  await $`sudo youtube-dl -U`;
-}
