e27b9df2009973245f88cc4fe0b00d2395b9e21f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-07 16:08:05 -0800

    chore: add todo

--- script/lib/fmt.ts
+++ script/lib/fmt.ts
@@ -1,5 +1,6 @@
 import { stdColors } from "./deps.ts";
 
+// TODO: look into removing this, and perhaps bringing the cliffy/colors module in which is fluent and more convenient to use
 export function blackOnYellow(what: string) {
   return stdColors.black(stdColors.bgYellow(what));
 }
