58241821503bd73b83d2a0013e5ec861ee63f0fc
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-09 16:30:09 -0800

    --wip-- [skip ci]

--- .web/deploy.ts
+++ .web/deploy.ts
@@ -1,11 +1,8 @@
 #!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net --allow-read --allow-run
 
-import { $, invariant } from "../script/lib/mod.ts";
+import { $ } from "../script/lib/mod.ts";
 
-invariant(
-  typeof (await $.which("deployctl")) !== "undefined",
-  `deployctl is required, install it with ${$.colors.magenta("pam install -a deployctl")}`,
-);
+await $.requireCommand("deployctl", "pam install -a deployctl");
 
 await $`deployctl deploy --project dotfiles-andrewbrey --prod serve.ts`.cwd(
   $.$dirname(import.meta.url),
