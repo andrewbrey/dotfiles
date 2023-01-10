d7465a12d428a7fa46a43e573d3794a94044b2c7
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-09 21:41:45 -0800

    --wip-- [skip ci]

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -16,13 +16,13 @@ export * as stdNodeFS from "https://deno.land/std@0.171.0/node/fs.ts";
 export * as stdNodeOS from "https://deno.land/std@0.171.0/node/os.ts";
 export * as stdNodeUtil from "https://deno.land/std@0.171.0/node/util.ts";
 export * as stdSemver from "https://deno.land/std@0.171.0/semver/mod.ts";
-export * as cliffyAnsi from "https://deno.land/x/cliffy@v0.25.6/ansi/mod.ts";
-export * as cliffyCmd from "https://deno.land/x/cliffy@v0.25.6/command/mod.ts";
-export * as cliffyTable from "https://deno.land/x/cliffy@v0.25.6/table/mod.ts";
-export * as dax from "https://deno.land/x/dax@0.22.0/mod.ts";
+export * as cliffyAnsi from "https://deno.land/x/cliffy@v0.25.7/ansi/mod.ts";
+export * as cliffyCmd from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+export * as cliffyTable from "https://deno.land/x/cliffy@v0.25.7/table/mod.ts";
+export * as dax from "https://deno.land/x/dax@0.23.0/mod.ts";
 export * as puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 export { default as strCase } from "npm:case@1.6.3";
 export * as dateFns from "npm:date-fns@2.29.3";
 export { default as handlebars } from "npm:handlebars@4.7.7";
 // @deno-types="npm:@types/user-agents@1.0.2"
-export { default as UserAgent } from "npm:user-agents@1.0.1252";
+export { default as UserAgent } from "npm:user-agents@1.0.1254";
