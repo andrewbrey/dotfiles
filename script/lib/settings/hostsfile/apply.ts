23d57c2fc1c1b0035df82743b49b3e1224803113
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-13 21:40:46 -0800

    refactor: allow homelab host script

--- script/lib/settings/hostsfile/apply.ts
+++ script/lib/settings/hostsfile/apply.ts
@@ -12,8 +12,18 @@ const sudoersEntry = $.path.join(sudoersDir, "hostsfile");
 if (await $.missing(sudoersEntry)) {
   invariant(await $.exists(sudoersDir), `missing required directory ${sudoersDir}`);
 
+  const homelabHostsScript = $.path.join(
+    $.env.STANDARD_DIRS.DOT_DOTS_SETTINGS,
+    "hostsfile",
+    ".homelab-hosts.sh",
+  );
+
   await $.raw`sudo tee ${sudoersEntry}`.stdin(
-    $.strings.asBytes(`${$.env.USER} ALL = (root) NOPASSWD: sudoedit /etc/hosts\n`),
+    $.strings.asBytes($.dedent`
+			${$.env.USER} ALL = (root) NOPASSWD: sudoedit /etc/hosts
+			${$.env.USER} ALL = (root) NOPASSWD: ${$.path.join(homelabHostsScript)}
+
+		`),
   );
 
   await $`sudo chmod 0440 ${sudoersEntry}`;
