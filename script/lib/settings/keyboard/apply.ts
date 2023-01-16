1df822e4c82bc990e6341cc98043adf3940a1379
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-16 11:23:59 -0800

    feat: make keyboard settings idempotent

--- script/lib/settings/keyboard/apply.ts
+++ script/lib/settings/keyboard/apply.ts
@@ -21,14 +21,25 @@ if ($.env.OS === "darwin") {
     //      https://superuser.com/questions/79822/how-to-swap-the-fn-use-of-function-keys-on-an-apple-keyboard-in-linux
 
     const fnKeyBehaviorCurrentSession = "/sys/module/hid_apple/parameters/fnmode";
+    const sessionSetting = "0";
     if (await $.exists(fnKeyBehaviorCurrentSession)) {
-      await $.raw`sudo tee ${fnKeyBehaviorCurrentSession}`.stdinText("0");
+      const currentSetting = await $.raw`sudo cat ${fnKeyBehaviorCurrentSession}`.text();
+
+      if (currentSetting?.trim() !== sessionSetting) {
+        await $.raw`sudo tee ${fnKeyBehaviorCurrentSession}`.stdinText(sessionSetting);
+      }
     }
 
     const fnKeyBehaviorPersist = "/etc/modprobe.d/hid_apple.conf";
+    const persistSetting = "options hid_apple fnmode=2";
+    if (await $.exists(fnKeyBehaviorPersist)) {
+      const currentSetting = await $.raw`sudo cat ${fnKeyBehaviorPersist}`.text();
+      if (currentSetting?.trim() === persistSetting) Deno.exit(0);
+    }
+
     await $.raw`sudo mkdir -p ${$.path.dirname(fnKeyBehaviorPersist)}`;
     await $.raw`sudo touch ${fnKeyBehaviorPersist}`;
-    await $.raw`sudo tee ${fnKeyBehaviorPersist}`.stdinText("options hid_apple fnmode=2");
+    await $.raw`sudo tee ${fnKeyBehaviorPersist}`.stdinText(persistSetting);
 
     await $`sudo update-initramfs -u`;
   }
