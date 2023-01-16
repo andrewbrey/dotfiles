7dca87023f0874021d4ba12be59c1b7919e282b9
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-01-15 21:28:13 -0800

    feat: address todo

--- script/lib/apps/python/install.ts
+++ script/lib/apps/python/install.ts
@@ -14,12 +14,9 @@ if (await $.commandMissing("python3") || await $.commandMissing("pip3")) {
   }
 }
 
-if ($.env.OS === "linux") {
-  // TODO: is this needed on mac too?
-  const python = await $.which("python");
-  const python3 = await $.which("python3");
-  if (!python && python3) await linkBinaryToUserPath(python3, "python");
-}
+const python = await $.which("python");
+const python3 = await $.which("python3");
+if (!python && python3) await linkBinaryToUserPath(python3, "python");
 
 const versionOutput = await $`python3 --version`.text(); // Python 3.10.6
 const version = versionOutput.split(" ")?.at(1) ?? "";
