8305cac5d8e3f735fe2391164df19e3f0d93566c
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-02-02 14:43:06 -0800

    feat: add gum to installer

--- .web/script.sh
+++ .web/script.sh
@@ -6,21 +6,24 @@ if [ -d "${HOME}/.dots" ]; then
 	exit 0
 fi
 
+GUM_BIN="$(command -v gum || echo '')"
+
+if [ -z "${GUM_BIN}" ]; then
+	GUM_TMP="$(mktemp -d)"
+	GUM_BIN="${GUM_TMP}/gum"
+
+	curl -fsSL "https://bina.egoist.dev/charmbracelet/gum?dir=${GUM_TMP}" | sh
+fi
+
 passphrase="${DOTFILES_PASSPHRASE}"
 personal="${PERSONAL_MACHINE}"
 
 if [ -z "${passphrase}" ]; then
-	read -p "dotfiles passphrase? " passphrase
+	passphrase="$(${GUM_BIN} input --password --placeholder 'dotfiles passphrase?')"
 fi
 
 if [ -z "${personal}" ]; then
-	read -p "personal machine (y/n)? " personal_response
-
-	if [ "${personal_response}" = "y" ]; then
-		personal='true'
-	else
-		personal='false'
-	fi
+	personal="$(${GUM_BIN} confirm 'personal machine?' --default='Yes' && echo 'true' || echo 'false')"
 fi
 
 git clone --depth=1 https://github.com/andrewbrey/dotfiles.git "${HOME}/dotfiles"
