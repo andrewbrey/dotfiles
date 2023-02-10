c8fd987be2ccc9d3e63498c41708a24a24105f8e
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-02-10 11:30:21 -0800

    feat: add ripgrep

---
+++ script/lib/apps/ripgrep/install.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { type InstallerMeta, pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+await $.onMac(async () => {
+	if (await pamkit.brewAppMissing("ripgrep")) {
+		await $`brew install ripgrep`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+	}
+});
+
+await $.onLinux(async () => {
+	if (await $.commandMissing("rg")) {
+		await $`sudo apt install -y ripgrep`;
+	}
+});
+
+const versionOutput = await $`rg --version`.lines(); // ripgrep 13.0.0\n....
+const version = versionOutput?.at(0)?.split(" ")?.at(1) ?? "";
+
+const meta: InstallerMeta = {
+	name: $.$dirname(import.meta.url, true),
+	path: $.$dirname(import.meta.url),
+	type: "installed-managed",
+	version,
+	lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, pamkit.constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/ripgrep/remove.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+
+await $.onMac(async () => {
+	if (await pamkit.brewAppInstalled("ripgrep")) {
+		await $`brew uninstall ripgrep`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+	}
+});
+
+await $.onLinux(async () => {
+	if (await $.commandExists("rg")) {
+		await $`sudo apt purge -y ripgrep`;
+	}
+});
+
+if (await $.exists(dotAppPath)) {
+	await Deno.remove(dotAppPath, { recursive: true });
+}
