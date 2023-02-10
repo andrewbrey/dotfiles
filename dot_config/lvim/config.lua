9505a7a97595ee330a3415db8f85010de7ba0238
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-02-10 12:50:25 -0800

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -12,6 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
 lvim.colorscheme = "lunar"
+lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
