dd6c5632a064f54d261279ba21bc90e503d46510
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-02-19 14:56:00 -0800

    feat: add lossless-cut

---
+++ script/lib/apps/lossless-cut/install.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { type InstallerMeta, pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+await $.onMac(async () => {
+	if (await pamkit.brewAppMissing("losslesscut")) {
+		await $`brew install --cask losslesscut`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+	}
+});
+
+await $.onLinux(async () => {
+	if (await $.commandMissing("losslesscut")) {
+		await $.requireCommand("snap", "pam install -a snapd");
+
+		await $`sudo snap install losslesscut`;
+	}
+});
+
+const meta: InstallerMeta = {
+	name: $.$dirname(import.meta.url, true),
+	path: $.$dirname(import.meta.url),
+	type: "installed-managed",
+	version: "",
+	lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, pamkit.constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/lossless-cut/remove.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+
+await $.onMac(async () => {
+	if (await pamkit.brewAppInstalled("losslesscut")) {
+		await $`brew uninstall --cask losslesscut`.env({ HOMEBREW_NO_ANALYTICS: "1" });
+	}
+});
+
+await $.onLinux(async () => {
+	if (await $.commandExists("losslesscut")) {
+		await $.requireCommand("snap", "pam install -a snapd");
+
+		await $`sudo snap remove losslesscut`;
+	}
+});
+
+if (await $.exists(dotAppPath)) {
+	await Deno.remove(dotAppPath, { recursive: true });
+}
