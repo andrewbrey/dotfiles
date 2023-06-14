f5e1660bf0d55fb22671106de139faf9fd9234a0
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-06-14 15:22:55 -0700

    chore: add write permission to chezmoi init

--- script/lib/init/99-chezmoi-init.ts
+++ script/lib/init/99-chezmoi-init.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net=deno.land --allow-read --allow-run
+#!/usr/bin/env -S deno run --allow-sys --unstable --allow-env --allow-net=deno.land --allow-read --allow-run --allow-write
 
 // -----
 // NOTE: this script should be the last to run; ensure it's
