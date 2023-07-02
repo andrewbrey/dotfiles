923944488ebefc91c7ac43019102213fe6ef7993
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-07-02 10:52:48 -0700

    chore: bump deps

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -13,9 +13,9 @@ export type {
 	LogRecord,
 } from "https://deno.land/std@0.192.0/log/mod.ts";
 export * as stdSemver from "https://deno.land/std@0.192.0/semver/mod.ts";
-export * as cliffyAnsi from "https://deno.land/x/cliffy@v0.25.7/ansi/mod.ts";
-export * as cliffyCmd from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
-export * as cliffyTable from "https://deno.land/x/cliffy@v0.25.7/table/mod.ts";
+export * as cliffyAnsi from "https://deno.land/x/cliffy@v1.0.0-rc.1/ansi/mod.ts";
+export * as cliffyCmd from "https://deno.land/x/cliffy@v1.0.0-rc.1/command/mod.ts";
+export * as cliffyTable from "https://deno.land/x/cliffy@v1.0.0-rc.1/table/mod.ts";
 export * as dax from "https://deno.land/x/dax@0.32.0/mod.ts";
 export * as puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 export * as stdNodeFS from "node:fs";
@@ -24,4 +24,4 @@ export { default as strCase } from "npm:case@1.6.3";
 export * as dateFns from "npm:date-fns@2.30.0";
 export { default as handlebars } from "npm:handlebars@4.7.7";
 // @deno-types="npm:@types/user-agents@1.0.2"
-export { default as UserAgent } from "npm:user-agents@1.0.1424";
+export { default as UserAgent } from "npm:user-agents@1.0.1428";
