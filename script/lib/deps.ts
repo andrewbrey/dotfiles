9acd633c303cd5275c436c522292033210f553fd
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-12-06 20:32:29 -0800

    chore: bump deps

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -26,4 +26,4 @@ export { default as strCase } from "npm:case@1.6.3";
 export * as dateFns from "npm:date-fns@3.0.0-rc.2";
 export { default as handlebars } from "npm:handlebars@4.7.8";
 // @deno-types="npm:@types/user-agents@1.0.4"
-export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.40";
+export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.42";
