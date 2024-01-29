806c301c18c140a9052b3387f88321c38a2764d3
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-01-28 22:15:22 -0800

    chore: bump deps

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -18,7 +18,7 @@ export * as stdSemver from "https://deno.land/std@0.213.0/semver/mod.ts";
 export * as cliffyAnsi from "https://deno.land/x/cliffy@v1.0.0-rc.3/ansi/mod.ts";
 export * as cliffyCmd from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 export * as cliffyTable from "https://deno.land/x/cliffy@v1.0.0-rc.3/table/mod.ts";
-export * as dax from "https://deno.land/x/dax@0.37.1/mod.ts";
+export * as dax from "https://deno.land/x/dax@0.38.0/mod.ts";
 export * as puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 export * as stdNodeFS from "node:fs";
 export * as stdNodeUtil from "node:util";
@@ -26,4 +26,4 @@ export { default as strCase } from "npm:case@1.6.3";
 export * as dateFns from "npm:date-fns@3.3.1";
 export { default as handlebars } from "npm:handlebars@4.7.8";
 // @deno-types="npm:@types/user-agents@1.0.4"
-export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.94";
+export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.95";
