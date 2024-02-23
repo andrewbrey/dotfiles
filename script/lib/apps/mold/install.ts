1892cef05e45435c72da267f19a9792684470cf0
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-02-22 20:47:01 -0800

    feat: add mold

---
+++ script/lib/apps/mold/install.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S deno run --allow-sys --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { type InstallerMeta, pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+if (await $.commandMissing("mold")) {
+	await $.requireCommand("eget", "pam install -a eget");
+
+	const binaryPath = $.path.join(dotAppPath, "mold");
+
+	await $`eget --to ${binaryPath} rui314/mold`.env({
+		EGET_GITHUB_TOKEN: $.env.GH_TOKEN,
+	});
+
+	await pamkit.linkBinaryToUserPath(binaryPath, "mold");
+}
+
+const versionLine = await $`mold --version`.text(); // mold 2.4.0 (3d9....
+const version = versionLine.split(" ").at(1);
+
+const meta: InstallerMeta = {
+	name: $.$dirname(import.meta.url, true),
+	path: $.$dirname(import.meta.url),
+	type: "installed-manual",
+	version,
+	lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, pamkit.constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/mold/update.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S deno run --allow-sys --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const [meta] = await pamkit.getInstallerMetas(new Set([$.$dirname(import.meta.url, true)]));
+if (await $.commandExists("mold")) {
+	await $.requireCommand("eget", "pam install -a eget");
+
+	const binaryPath = $.path.join(dotAppPath, "mold");
+
+	await $`eget --to ${binaryPath} rui314/mold`.env({
+		EGET_GITHUB_TOKEN: $.env.GH_TOKEN,
+	});
+
+	await pamkit.linkBinaryToUserPath(binaryPath, "mold");
+
+	meta.lastCheck = Date.now();
+}
+
+const versionLine = await $`mold --version`.text(); // mold 2.4.0 (3d9....
+const version = versionLine.split(" ").at(1);
+
+meta.version = version;
+
+const metaManifestPath = $.path.join(dotAppPath, pamkit.constants.metaManifestName);
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
