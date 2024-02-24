3b5b77c5d8b5d181b518451c090a894d1f236b3b
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-02-24 00:06:53 -0800

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "Dracula"
+config.color_scheme = "Arthur"
+config.default_cursor_style = 'SteadyBar'
 config.window_background_image = wezterm.config_dir .. "/assets/waves.gif"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 1.0
 config.window_background_image_hsb = {
 	brightness = 0.05,
 	hue = 1.0,
