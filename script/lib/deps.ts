416f26b04d90aafc47e37862dcf6a800493160fa
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-03-13 16:58:48 -0700

    chore: bump deps

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -13,13 +13,13 @@ export * as dax from "jsr:@david/dax@0.39.2";
 export { intersect as stdIntersect } from "jsr:@std/collections@0.219.1/intersect";
 export * as stdFS from "jsr:@std/fs@0.219.1";
 export * as stdLog from "jsr:@std/log@0.219.1";
-export type { FormatterFunction, Logger, LogRecord } from "jsr:@std/log@0.219.1";
+export type { FormatterFunction, LogRecord, Logger } from "jsr:@std/log@0.219.1";
 export * as stdPath from "jsr:@std/path@0.219.1";
 export * as stdSemver from "jsr:@std/semver@0.219.1";
 export * as stdNodeFS from "node:fs";
 export * as stdNodeUtil from "node:util";
 export { default as strCase } from "npm:case@1.6.3";
-export * as dateFns from "npm:date-fns@3.3.1";
+export * as dateFns from "npm:date-fns@3.4.0";
 export { default as handlebars } from "npm:handlebars@4.7.8";
 // @deno-types="npm:@types/user-agents@1.0.4"
-export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.136";
+export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.140";
