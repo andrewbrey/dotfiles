22381b42fe5b3db620a4fdbad1e9cd94e54aed20
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-03-14 18:01:41 -0700

    chore: bump deps and revert to https specifiers for now

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -5,21 +5,25 @@
  *
  * deno run --allow-read=. --allow-write=. --allow-net https://deno.land/x/udd/main.ts script/lib/deps.ts .web/serve.ts
  */
+export { intersect as stdIntersect } from "https://deno.land/std@0.220.0/collections/intersect.ts";
+export * as stdFS from "https://deno.land/std@0.220.0/fs/mod.ts";
+export * as stdLog from "https://deno.land/std@0.220.0/log/mod.ts";
+export type {
+	FormatterFunction,
+	Logger,
+	LogRecord,
+} from "https://deno.land/std@0.220.0/log/mod.ts";
+export * as stdPath from "https://deno.land/std@0.220.0/path/mod.ts";
+export * as stdSemver from "https://deno.land/std@0.220.0/semver/mod.ts";
 export * as cliffyAnsi from "https://deno.land/x/cliffy@v1.0.0-rc.3/ansi/mod.ts";
 export * as cliffyCmd from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 export * as cliffyTable from "https://deno.land/x/cliffy@v1.0.0-rc.3/table/mod.ts";
+export * as dax from "https://deno.land/x/dax@0.39.2/mod.ts";
 export * as puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
-export * as dax from "jsr:@david/dax@0.39.2";
-export { intersect as stdIntersect } from "jsr:@std/collections@0.219.1/intersect";
-export * as stdFS from "jsr:@std/fs@0.219.1";
-export * as stdLog from "jsr:@std/log@0.219.1";
-export type { FormatterFunction, LogRecord, Logger } from "jsr:@std/log@0.219.1";
-export * as stdPath from "jsr:@std/path@0.219.1";
-export * as stdSemver from "jsr:@std/semver@0.219.1";
 export * as stdNodeFS from "node:fs";
 export * as stdNodeUtil from "node:util";
 export { default as strCase } from "npm:case@1.6.3";
 export * as dateFns from "npm:date-fns@3.4.0";
 export { default as handlebars } from "npm:handlebars@4.7.8";
 // @deno-types="npm:@types/user-agents@1.0.4"
-export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.140";
+export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.141";
