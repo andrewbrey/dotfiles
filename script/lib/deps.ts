a7acbdec486b58c98f26a33f4a61b522e86d3fc3
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-04-13 09:49:32 -0700

    chore: bump deps

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -5,16 +5,16 @@
  *
  * deno run --allow-read=. --allow-write=. --allow-net https://deno.land/x/udd/main.ts script/lib/deps.ts .web/serve.ts
  */
-export { intersect as stdIntersect } from "https://deno.land/std@0.221.0/collections/intersect.ts";
-export * as stdFS from "https://deno.land/std@0.221.0/fs/mod.ts";
-export * as stdLog from "https://deno.land/std@0.221.0/log/mod.ts";
+export { intersect as stdIntersect } from "https://deno.land/std@0.222.1/collections/intersect.ts";
+export * as stdFS from "https://deno.land/std@0.222.1/fs/mod.ts";
+export * as stdLog from "https://deno.land/std@0.222.1/log/mod.ts";
 export type {
 	FormatterFunction,
 	Logger,
 	LogRecord,
-} from "https://deno.land/std@0.221.0/log/mod.ts";
-export * as stdPath from "https://deno.land/std@0.221.0/path/mod.ts";
-export * as stdSemver from "https://deno.land/std@0.221.0/semver/mod.ts";
+} from "https://deno.land/std@0.222.1/log/mod.ts";
+export * as stdPath from "https://deno.land/std@0.222.1/path/mod.ts";
+export * as stdSemver from "https://deno.land/std@0.222.1/semver/mod.ts";
 export * as cliffyAnsi from "https://deno.land/x/cliffy@v1.0.0-rc.4/ansi/mod.ts";
 export * as cliffyCmd from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 export * as cliffyTable from "https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts";
@@ -26,4 +26,4 @@ export { default as strCase } from "npm:case@1.6.3";
 export * as dateFns from "npm:date-fns@3.6.0";
 export { default as handlebars } from "npm:handlebars@4.7.8";
 // @deno-types="npm:@types/user-agents@1.0.4"
-export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.167";
+export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.171";
