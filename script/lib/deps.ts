772e9f39096a023443069c0bc000b224de2a97b8
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-08-22 19:01:13 -0700

    chore: use jsr deps

--- script/lib/deps.ts
+++ script/lib/deps.ts
@@ -5,25 +5,21 @@
  *
  * deno run --allow-read=. --allow-write=. --allow-net https://deno.land/x/udd/main.ts script/lib/deps.ts .web/serve.ts
  */
-export { intersect as stdIntersect } from "https://deno.land/std@0.224.0/collections/intersect.ts";
-export * as stdFS from "https://deno.land/std@0.224.0/fs/mod.ts";
-export * as stdLog from "https://deno.land/std@0.224.0/log/mod.ts";
-export type {
-	FormatterFunction,
-	Logger,
-	LogRecord,
-} from "https://deno.land/std@0.224.0/log/mod.ts";
-export * as stdPath from "https://deno.land/std@0.224.0/path/mod.ts";
-export * as stdSemver from "https://deno.land/std@0.224.0/semver/mod.ts";
 export * as cliffyAnsi from "https://deno.land/x/cliffy@v1.0.0-rc.4/ansi/mod.ts";
 export * as cliffyCmd from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 export * as cliffyTable from "https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts";
 export * as puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 export * as dax from "jsr:@david/dax@0.40.0";
+export { intersect as stdIntersect } from "jsr:@std/collections@1.0.5/intersect";
+export * as stdFS from "jsr:@std/fs@1.0.1";
+export * as stdLog from "jsr:@std/log@0.224.5";
+export type { FormatterFunction, Logger, LogRecord } from "jsr:@std/log@0.224.5";
+export * as stdPath from "jsr:@std/path@1.0.2";
+export * as stdSemver from "jsr:@std/semver@1.0.1";
 export * as stdNodeFS from "node:fs";
 export * as stdNodeUtil from "node:util";
-export { default as strCase } from "npm:case@1.6.3";
+export { default as strCase } from "npm:case@1.6.3"; // TODO: use @std/text
 export * as dateFns from "npm:date-fns@3.6.0";
 export { default as handlebars } from "npm:handlebars@4.7.8";
 // @deno-types="npm:@types/user-agents@1.0.4"
-export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.228";
+export { default as UserAgent } from "npm:user-agents@2.0.0-alpha.302";
