db220add54181565a66f1a1d7e2f9f5a22a82ca6
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-08-31 09:38:33 -0700

    feat: add linear

---
+++ script/lib/apps/linear/install.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S deno run --allow-sys --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { type InstallerMeta, pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const name = $.$dirname(import.meta.url, true);
+if (await $.commandMissing(name)) {
+	await pamkit.createAndLinkNativefierApp({
+		appName: name,
+		displayName: "Linear",
+		website: "https://linear.app",
+		frame: false,
+	});
+}
+
+const meta: InstallerMeta = {
+	name,
+	path: $.$dirname(import.meta.url),
+	type: "installed-manual",
+	version: "0.0.0",
+	lastCheck: Date.now(),
+};
+const metaManifestPath = $.path.join(dotAppPath, pamkit.constants.metaManifestName);
+
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
---
+++ script/lib/apps/linear/outdated.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S deno run --allow-sys --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { pamkit } from "../_cli/pamkit.ts";
+
+const [meta] = await pamkit.getInstallerMetas(new Set([$.$dirname(import.meta.url, true)]));
+
+const outdatedCheck = await pamkit.wrapOutdatedCheck(meta, 3, async () => {
+	return "1.0.0";
+});
+
+await $`echo ${JSON.stringify(outdatedCheck)}`.printCommand(false);
