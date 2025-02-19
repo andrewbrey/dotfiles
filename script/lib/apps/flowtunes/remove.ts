85be349750330b89a45e26e7e2c4918385f0051a
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2025-02-19 11:55:41 -0800

    feat: add flowtunes

---
+++ script/lib/apps/flowtunes/remove.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S deno run --allow-sys --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+
+const name = $.$dirname(import.meta.url, true);
+if (await $.commandExists(name)) {
+	await pamkit.unlinkNativefierApp(name);
+}
+
+if (await $.exists(dotAppPath)) {
+	await Deno.remove(dotAppPath, { recursive: true });
+}
---
+++ script/lib/apps/flowtunes/update.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S deno run --allow-sys --allow-env --allow-net --allow-read --allow-write --allow-run
+
+import { $ } from "../../mod.ts";
+import { pamkit } from "../_cli/pamkit.ts";
+
+const dotAppPath = $.path.join($.$dirname(import.meta.url), pamkit.constants.appArtifactsDir);
+await $.fs.ensureDir(dotAppPath);
+
+const [meta] = await pamkit.getInstallerMetas(new Set([$.$dirname(import.meta.url, true)]));
+
+const name = $.$dirname(import.meta.url, true);
+if (await $.commandExists(name)) {
+	await pamkit.createAndLinkNativefierApp({
+		appName: name,
+		displayName: "FlowTunes",
+		website: "https://flowtunes.app",
+		frame: false,
+	});
+
+	meta.lastCheck = Date.now();
+}
+
+const metaManifestPath = $.path.join(dotAppPath, pamkit.constants.metaManifestName);
+await Deno.writeTextFile(metaManifestPath, JSON.stringify(meta, null, 2));
